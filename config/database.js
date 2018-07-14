79f5028f9ab0265b73d9c750304277b164b23f9a
Wasiq <nwasiq@gmail.com>
2018-07-14 15:53:56 +0500

    no message

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
     localDatabase: 'mongodb://localhost/simulens2db',
+    ec2Db: 'mongodb://wasiq2:wasiq2@ec2-13-57-59-4.us-west-1.compute.amazonaws.com:27017/careemdb',
     secret: 'secretkey'
   }
